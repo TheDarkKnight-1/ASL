a88a785cad53ed90acd2f31249c64b50ed914326
TheDarkKnight-1 <chandanapmishra2017@gmail.com>
2021-03-27 10:16:13 +0000

    updated to 2.0

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ const TOPK = 10;
 
 const predictionThreshold = 0.98
 
-var words = ["alexa", "hello", "other"]
-// var words = ["alexa", "hello", "what is", "the weather", "the time",
+var words = ["Hey Google", "hello", "other"]
+// var words = ["Hey Google", "hello", "what is", "the weather", "the time",
 //"add","eggs","to the list","five","feet","in meters","tell me","a joke", "bye", "other"]
 
 
@@ -146,7 +146,7 @@ class Main {
         // if wake word has not been trained
         if(exampleCount[0] == 0){
           alert(
-            `You haven't added examples for the wake word ALEXA`
+            `You haven't added examples for the wake word Hey Google`
             )
           return
         }
@@ -192,12 +192,12 @@ class Main {
       // check if user has added atleast one terminal word
       if(words.length > 3 && endWords.length == 1){
         console.log('no terminal word added')
-        alert(`You have not added any terminal words.\nCurrently the only query you can make is "Alexa, hello".\n\nA terminal word is a word that will appear in the end of your query.\nIf you intend to ask "What's the weather" & "What's the time" then add "the weather" and "the time" as terminal words. "What's" on the other hand is not a terminal word.`)
+        alert(`You have not added any terminal words.\nCurrently the only query you can make is "Hey Google, hello".\n\nA terminal word is a word that will appear in the end of your query.\nIf you intend to ask "What's the weather" & "What's the time" then add "the weather" and "the time" as terminal words. "What's" on the other hand is not a terminal word.`)
         return
       }
 
       if(words.length == 3 && endWords.length ==1){
-        var proceed = confirm("You have not added any words.\n\nThe only query you can currently make is: 'Alexa, hello'")
+        var proceed = confirm("You have not added any words.\n\nThe only query you can currently make is: 'Hey Google, hello'")
 
         if(!proceed) return
       }
@@ -499,20 +499,20 @@ class TextToSpeech{
 
   speak(word){
 
-    if(word == 'alexa'){
+    if(word == 'Hey Google'){
       console.log("clear para")
       this.clearPara(true);
 
       setTimeout(() => {
-        // if no query detected after alexa is signed
+        // if no query detected after Hey Google is signed
         if(this.currentPredictedWords.length == 1){
           this.clearPara(false)
         }
       }, this.waitTimeForQuery)
     } 
 
-    if(word != 'alexa' && this.currentPredictedWords.length == 0){
-      console.log("first word should be alexa")
+    if(word != 'Hey Google' && this.currentPredictedWords.length == 0){
+      console.log("first word should be Hey Google")
       console.log(word)
       return
     }
